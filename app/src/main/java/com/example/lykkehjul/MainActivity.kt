1549ab60d03612a1ef259d17660f88618c3c77e6
Numan Bashir <sportybashir@gmail.com>
2021-11-01 11:05:47 +0100

    Knap, flere textviews

--- app/src/main/java/com/example/lykkehjul/MainActivity.kt
+++ app/src/main/java/com/example/lykkehjul/MainActivity.kt
@@ -2,10 +2,37 @@ package com.example.lykkehjul
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
+import android.widget.Button
+import android.widget.TextView
+import kotlin.random.Random
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        // List words
+        val words = listOf("kotlin", "java", "python", "swift")
+        val randomElement = words.random()
+        println(randomElement)
+        Log.i("random element from array", randomElement)
+
+        // Convert string into CharArray
+        val chars: CharArray = randomElement.toCharArray()
+        println(chars.contentToString())
+
+        val startSpilKnap: Button = findViewById(R.id.startSpilKnap)
+        val ordTilView: TextView = findViewById(R.id.ordTilView)
+        var antalLiv: TextView = findViewById(R.id.antalLiv)
+
+        startSpilKnap.setOnClickListener {
+            ordTilView.setText(randomElement)
+            //antalLiv = antalLiv - 1
+
+        }
+
+
+
     }
 }
