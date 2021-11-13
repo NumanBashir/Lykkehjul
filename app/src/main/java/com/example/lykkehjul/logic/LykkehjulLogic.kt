2f0b053c2b4b5384d306a7d76a1bccdbdfd7b0ac
numanbashir <sportybashir@gmail.com>
2021-11-13 21:40:42 +0100

    Optæl point

--- app/src/main/java/com/example/lykkehjul/logic/LykkehjulLogic.kt
+++ app/src/main/java/com/example/lykkehjul/logic/LykkehjulLogic.kt
@@ -7,16 +7,14 @@ object LykkehjulLogic {
         val ordArray: CharArray = ord.toCharArray()
 
         if (validateGætBogstavLength(bogstav)) {
-            if (hemmeligtOrd.contains(bogstav)) {
-                var index = 0
+            var index = 0
 
-                // Lav funktion for hvad der sker i løkken
-                while (index < hemmeligtOrd.length) {
-                    if (hemmeligtOrd.get(index) == bogstav.get(0)) {
-                        ordArray[index] = bogstav.get(0)
-                    }
-                    index++
+            // Lav funktion for hvad der sker i løkken
+            while (index < hemmeligtOrd.length) {
+                if (hemmeligtOrd.get(index) == bogstav.get(0)) {
+                    ordArray[index] = bogstav.get(0)
                 }
+                index++
             }
         }
 
@@ -25,6 +23,10 @@ object LykkehjulLogic {
 
     fun validateGætBogstavLength(bogstav: String): Boolean {
         return bogstav.length > 0
+    }
+
+    fun erBogstavIHemmeligOrd(hemmeligtOrd: String, bogstav: String): Boolean {
+        return hemmeligtOrd.contains(bogstav)
     }
 
 }
--- app/src/main/java/com/example/lykkehjul/view/PlayGame.kt
+++ app/src/main/java/com/example/lykkehjul/view/PlayGame.kt
@@ -20,21 +20,26 @@ class PlayGame : AppCompatActivity() {
     lateinit var gætBogstav: EditText
     lateinit var drejHjulKnap: Button
     lateinit var wheelOutcomeDisplay: TextView
+    lateinit var pointsNumber: TextView
 
     private val gameManager = GameEvents()
 
     var lives = 5
+    var points = 0
     var visHemmeligOrd = ""
     var rigtigeGæt = mutableListOf<String>()
     var secretOrd = ""
     var hemmeligtOrd = ""
-    var drejEllerIndtast = true
+    var drejEllerIndtastState = true
+    var randomOutcome = ""
+
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_play_game)
 
         ord = findViewById(R.id.ord)
+        pointsNumber = findViewById(R.id.pointsNumber)
 
 
         // Load words
@@ -67,14 +72,14 @@ class PlayGame : AppCompatActivity() {
     fun drejHjul() {
         // Spin wheel
         drejHjulKnap.setOnClickListener {
-            if (drejEllerIndtast) {
+            if (drejEllerIndtastState) {
                 // Get data for loadWheel
                 val wheelOutcomes = Memory().loadWheel()
                 // Set textView to random element from list
-                val randomOutcome = wheelOutcomes.random().toString()
+                randomOutcome = wheelOutcomes.random().toString()
                 wheelOutcomeDisplay.setText(randomOutcome)
                 Toast.makeText(applicationContext,"Indtast venligst bogstav",Toast.LENGTH_SHORT).show()
-                drejEllerIndtast = false
+                drejEllerIndtastState = false
             } else {
                 Toast.makeText(applicationContext,"Du har ikke indtastet et bogstav",Toast.LENGTH_SHORT).show()
             }
@@ -83,14 +88,50 @@ class PlayGame : AppCompatActivity() {
 
     fun guessLetter() {
         tastBogstav.setOnClickListener {
-            if (!drejEllerIndtast) {
+            if (!drejEllerIndtastState) {
                 if(TextUtils.isEmpty(gætBogstav.text.toString())) {
                     Toast.makeText(applicationContext,"Indtast et bogstav før du går videre",Toast.LENGTH_SHORT).show()
                 } else {
-                    ord.setText(String(LykkehjulLogic.guessLetter(ord.text.toString(), hemmeligtOrd, gætBogstav.text.toString())))
-                    gætBogstav.setText("")
-                    Toast.makeText(applicationContext,"Drej venligst hjulet",Toast.LENGTH_SHORT).show()
-                    drejEllerIndtast = true
+                    val brugerIndtastet = gætBogstav.text.toString()
+                    if (LykkehjulLogic.erBogstavIHemmeligOrd(hemmeligtOrd, brugerIndtastet)) {
+                        /*if (randomOutcome.contains("1.000kr") ||
+                            randomOutcome.contains("2.500kr") ||
+                            randomOutcome.contains("5.000kr") ||
+                            randomOutcome.contains("10.000kr") ||
+                            randomOutcome.contains("500kr") ||
+                            randomOutcome.contains("10kr")) {
+
+
+                            pointsNumber.setText((pointsNumber.text.toString() + 1000))
+
+                        }*/
+                            if(randomOutcome.contains("1.000kr")) {
+                                points += 1000
+                                pointsNumber.setText(points.toString())
+                            } else if(randomOutcome.contains("2.500kr")) {
+                                points += 2500
+                                pointsNumber.setText(points.toString())
+                            } else if(randomOutcome.contains("5.000kr")) {
+                                points += 5000
+                                pointsNumber.setText(points.toString())
+                            } else if(randomOutcome.contains("10.000kr")) {
+                                points += 10000
+                                pointsNumber.setText(points.toString())
+                            } else if(randomOutcome.contains("500kr")) {
+                                points += 500
+                                pointsNumber.setText(points.toString())
+                            } else if(randomOutcome.contains("10kr")) {
+                                points += 10
+                                pointsNumber.setText(points.toString())
+                            }
+
+                        ord.setText(String(LykkehjulLogic.guessLetter(ord.text.toString(), hemmeligtOrd, brugerIndtastet)))
+                        gætBogstav.setText("")
+                        Toast.makeText(applicationContext,"Drej venligst hjulet",Toast.LENGTH_SHORT).show()
+                    }
+
+
+                    drejEllerIndtastState = true
                 }
             } else {
                 Toast.makeText(applicationContext,"Du har ikke drejet hjulet endnu",Toast.LENGTH_SHORT).show()
